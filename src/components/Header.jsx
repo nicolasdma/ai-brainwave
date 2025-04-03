f8b57cbc333ee92fb39655df0f027f3f9a1e652f
Nicolás Demaria <nicolasdemaria123@gmail.com>
2025-04-03 17:27:30 -0500

    fix deploy - header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
-// import MenuSVG from "../assets/svg/MenuSvg";
-import MenuSVG from "../assets/svg/MenuSVG";
+import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Header = () => {
   const pathname = useLocation();
@@ -31,19 +31,19 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/9 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="brainwave" />
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
         <nav
-          className={`fixed top-[5rem] left-0 right-0 bottom-0 bg-b-8 lg:static lg:flex lg:mx-auto lg:bg-transparent ${
+          className={`${
             openNavigation ? "flex" : "hidden"
-          }`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
@@ -53,7 +53,7 @@ const Header = () => {
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
-                } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
@@ -63,6 +63,7 @@ const Header = () => {
               </a>
             ))}
           </div>
+
           <HamburgerMenu />
         </nav>
 
@@ -81,7 +82,7 @@ const Header = () => {
           px="px-3"
           onClick={toggleNavigation}
         >
-          <MenuSVG openNavigation={openNavigation} />
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
